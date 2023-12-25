3ded3587fbf2893e9cf9e4ea87b8fda087680a46
Hongtian Wang <i@codingoier.com>
2023-12-25 21:58:21 +0800

    fix dir Luogu

--- Luogu/P1494.cpp
+++ Luogu/P1494.cpp
@@ -1,5 +1,23 @@
 #include <cstdio>
+const int MaxN = 5e4 + 5;
+class question
+{
+    int id;
+    int l, r;
+    int answer;
+    friend bool operator<(const question &__x, const question &__y)
+    {
+    }
+};
+int n, m;
+int color[MaxN];
 int main()
 {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &color[i]);
+    }
+
     return 0;
 }
