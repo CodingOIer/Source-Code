980cf11b66ace16459405d9f10fddca2c4e82cf5
Hongtian Wang <i@codingoier.com>
2023-12-27 22:07:00 +0800

    fix CF1749C.cpp

--- Luogu/CF1749C.cpp
+++ Luogu/CF1749C.cpp
@@ -1,6 +1,68 @@
 #include <cstdio>
+#include <set>
 constexpr int MaxN = 1e2 + 5;
+int t;
+int n;
+int p[MaxN];
+std::multiset<int> set;
+void solve()
+{
+    scanf("%d", &n);
+    set.clear();
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &p[i]);
+    }
+    for (int k = 1; k <= n; k++)
+    {
+        set.clear();
+        for (int i = 1; i <= n; i++)
+        {
+            set.insert(p[i]);
+        }
+        int accept = true;
+        for (int i = 1; i <= k; i++)
+        {
+            for (; !set.empty() && !(*set.rbegin() <= k - i + 1);)
+            {
+                set.erase(*set.rbegin());
+            }
+            if (set.empty())
+            {
+                accept = false;
+                break;
+            }
+            set.erase(set.find(*set.rbegin()));
+            if (set.empty())
+            {
+                if (i == k)
+                {
+                    break;
+                }
+                else
+                {
+                    accept = false;
+                    break;
+                }
+            }
+            int tmp = *set.begin();
+            set.erase(set.find(tmp));
+            set.insert(tmp + k - i + 1);
+        }
+        if (!accept)
+        {
+            printf("%d\n", k - 1);
+            return;
+        }
+    }
+    printf("%d\n", n);
+}
 int main()
 {
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
     return 0;
 }
