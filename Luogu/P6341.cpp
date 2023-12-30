4c3e948b356604c8ee47bae76ae65cbb60085f33
CodingOIer <redefinition0726@163.com>
2023-12-30 15:05:46 +0800

    fix P6341.cpp

--- Luogu/P6341.cpp
+++ Luogu/P6341.cpp
@@ -1,7 +1,22 @@
+#include <cmath>
 #include <cstdio>
+#include <map>
 constexpr int MaxN = 1e3 + 5e2 + 5;
 int n;
+int x[MaxN];
+int y[MaxN];
 int main()
 {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d%d", &x[i], &y[i]);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+        }
+    }
     return 0;
 }
