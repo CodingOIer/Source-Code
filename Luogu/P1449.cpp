5302f59560b67e99cae58a43e09390fd219f0aae
CodingOIer <wanghongtiancodingoier@gmail.com>
2023-12-31 09:43:24 +0800

    fix P1449.cpp

--- Luogu/P1449.cpp
+++ Luogu/P1449.cpp
@@ -1,5 +1,21 @@
 #include <cstdio>
+#include <cstring>
+#include <stack>
+constexpr int MaxN = 5e1 + 5;
+int n;
+char s[MaxN];
+std::stack<int> stack;
 int main()
 {
+    scanf("%s", s + 1);
+    n = strlen(s + 1);
+    int tmp = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if ('0' <= s[i] && s[i] <= '9')
+        {
+            tmp = tmp * 10 + s[i] - '0';
+        }
+    }
     return 0;
 }
