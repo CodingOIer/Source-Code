5cf18cdd01888ec06b14079ae3e1edabd2b549ce
CodingOIer <wanghongtiancodingoier@outlook.com>
2024-03-08 17:03:13 +0800

    fix P4884.cpp

--- Luogu/P4884.cpp
+++ Luogu/P4884.cpp
@@ -1,17 +1,13 @@
-/**
- * @file Luogu\P4884.cpp
- * @author CodingOIer (redefinition0726@163.com)
- * @brief Luogu
- * @version 1.0
- * @date 2024-01-23
- *
- * @copyright Copyright (c) 2024
- *
- */
 #include <cmath>
 #include <cstdio>
 #include <unordered_map>
 long long k, m;
+inline long long times(long long x, long long y, long long p)
+{
+    long long z = (long double)x / p * y;
+    long long res = (unsigned long long)x * y - (unsigned long long)z * p;
+    return (res + p) % p;
+}
 long long fsp(long long x, long long y, long long p)
 {
     long long res = 1;
@@ -19,11 +15,9 @@ long long fsp(long long x, long long y, long long p)
     {
         if (y % 2 == 1)
         {
-            res *= x;
-            res %= p;
+            res = times(res, x, p);
         }
-        x *= x;
-        x %= p;
+        x = times(x, x, p);
         y /= 2;
     }
     return res;
@@ -32,11 +26,11 @@ long long bsgs(long long x, long long y, long long p)
 {
     std::unordered_map<long long, long long> map;
     long long m = sqrt(p) + 1;
-    for (int i = 0; i < m; i++)
+    for (long long i = 0; i < m; i++)
     {
-        map[y % (p - 1) * fsp(x, i, p) % p] = i;
+        map[times(y % (p - 1), fsp(x, i, p), p)] = i;
     }
-    for (int i = 1; i <= m; i++)
+    for (long long i = 1; i <= m; i++)
     {
         long long temp = fsp(x, i * m, p);
         if (map.count(temp))
@@ -46,9 +40,9 @@ long long bsgs(long long x, long long y, long long p)
     }
     return -1;
 }
-signed main()
+int main()
 {
     scanf("%lld%lld", &k, &m);
-    printf("%lld\n", bsgs(10, (9 * k + 1) % m, m));
+    printf("%lld\n", bsgs(10, (9ll * k + 1) % m, m));
     return 0;
 }
