8eaa6814662b89e86bf5e7462b80c967e0268645
CodingOIer <wanghongtiancodingoier@gmail.com>
2024-03-15 17:28:38 +0800

    fix P4513.cpp

--- Luogu/P4513.cpp
+++ Luogu/P4513.cpp
@@ -1,12 +1,15 @@
+#include <algorithm>
 #include <cstdio>
 constexpr int MaxN = 5e5 + 5;
 class node
 {
+  public:
     int x;
     int max;
     int sum;
     int l_max;
     int r_max;
+    bool son;
     node()
     {
         x = 0;
@@ -14,6 +17,7 @@ class node
         sum = 0;
         l_max = 0;
         r_max = 0;
+        son = false;
     }
     friend node operator+(const node &__x, const node &__y)
     {
@@ -30,6 +34,17 @@ class node
 int n, m;
 int p[MaxN];
 node tree[MaxN * 4];
+void build(int c, int s, int t)
+{
+    if (s == t)
+    {
+        tree[c].x = p[s];
+        tree[c].son = true;
+        tree[c].update();
+        return;
+    }
+    int mid = (s + t) / 2;
+}
 int main()
 {
     scanf("%d%d", &n, &m);
