e251718a9dd2f7e9b80dbc9a09a8b6d23abe069d
Hongtian Wang <i@codingoier.com>
2024-04-16 22:50:27 +0800

    craft P1533

--- Luogu/P1533.cpp
+++ Luogu/P1533.cpp
@@ -1,28 +1,123 @@
 #include <algorithm>
-#include <functional>
-#include <iostream>
+#include <cstdio>
 #include <vector>
+constexpr int MaxN = 3e5 + 5;
+class ques
+{
+  public:
+    int id;
+    int l, r, x;
+    ques(int a, int b, int c, int i)
+    {
+        l = a;
+        r = b;
+        x = c;
+        id = i;
+    }
+};
+int n, m;
+int answer[MaxN];
+int tree[MaxN * 4];
+std::pair<int, int> dog[MaxN];
+void change(int c, int s, int t, int w, int v)
+{
+    if (s == t)
+    {
+        tree[c] += v;
+        return;
+    }
+    int mid = (s + t) / 2;
+    if (w <= mid)
+    {
+        change(c * 2, s, mid, w, v);
+    }
+    else
+    {
+        change(c * 2 + 1, mid + 1, t, w, v);
+    }
+    tree[c] = tree[c * 2] + tree[c * 2 + 1];
+}
+int query(int c, int s, int t, int l, int r)
+{
+    if (l <= s && t <= r)
+    {
+        return tree[c];
+    }
+    int mid = (s + t) / 2;
+    int res = 0;
+    if (l <= mid)
+    {
+        res += query(c * 2, s, mid, l, r);
+    }
+    if (r > mid)
+    {
+        res += query(c * 2 + 1, mid + 1, t, l, r);
+    }
+    return res;
+}
+void calc(int l, int r, const std::vector<ques> &q)
+{
+    if (q.empty())
+    {
+        return;
+    }
+    if (l == r)
+    {
+        for (auto k : q)
+        {
+            answer[k.id] = dog[l].first;
+        }
+        return;
+    }
+    int mid = (l + r) / 2;
+    for (int i = l; i <= mid; i++)
+    {
+        change(1, 1, n, dog[i].second, 1);
+    }
+    std::vector<ques> left;
+    std::vector<ques> right;
+    for (auto k : q)
+    {
+        int sum = query(1, 1, n, k.l, k.r);
+        if (k.x <= sum)
+        {
+            left.push_back(k);
+        }
+        else
+        {
+            k.x -= sum;
+            right.push_back(k);
+        }
+    }
+    for (int i = l; i <= mid; i++)
+    {
+        change(1, 1, n, dog[i].second, -1);
+    }
+    calc(l, mid, left);
+    calc(mid + 1, r, right);
+}
 int main()
 {
-    // 关闭流同步，使cin和cout可以同时使用
-    std::ios_base::sync_with_stdio(false);
-    // 读取输入
-    long long n, m;
-    std::cin >> n >> m;
-    std::vector<long long> o(n), v;
-    for (int i = 0; i < n; i++)
-        std::cin >> o[i];
-    // 遍历输入的m个询问
-    for (int i = 0; i < m; i++)
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
     {
-        // 读取询问的参数
-        long long l, r, k;
-        std::cin >> l >> r >> k;
-        // 将o中l-1到r-1的元素赋值给v
-        v.assign(o.begin() + l - 1, o.begin() + r);
-        // 对v进行排序，并将第k个元素赋值给ans
-        std::nth_element(v.begin(), v.begin() + k - 1, v.end());
-        std::cout << v[k - 1] << std::endl;
+        int x;
+        scanf("%d", &x);
+        dog[i].first = x;
+        dog[i].second = i;
+    }
+    std::sort(dog + 1, dog + 1 + n);
+    std::vector<ques> q;
+    for (int i = 1; i <= m; i++)
+    {
+        int l, r, x;
+        scanf("%d%d%d", &l, &r, &x);
+        q.push_back(ques(l, r, x, i));
+    }
+    calc(1, n, q);
+    for (int i = 1; i <= m; i++)
+    {
+        printf("%d\n", answer[i]);
     }
     return 0;
 }
