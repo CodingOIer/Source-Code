e68498aabcc59181b7c4cde377db6908ef6ba8b3
Hongtian Wang <i@codingoier.com>
2024-05-03 17:31:39 +0800

    fix P1253.cpp

--- Luogu/P1253.cpp
+++ Luogu/P1253.cpp
@@ -1,69 +1,113 @@
 #include <algorithm>
 #include <cstdio>
-constexpr int MaxN = 2e6 + 5;
-constexpr long long Diff = 754324754324754234;
-int n, q;
+constexpr int MaxN = 1e6 + 5;
+constexpr long long Diff = 754324754324754324;
+int n, m;
+long long p[MaxN];
 long long tree[MaxN * 4];
 long long lazy_add[MaxN * 4];
-long long lazy_set[MaxN * 4];
-void downstream(int c, int s, int t)
+long long lazy_change[MaxN * 4];
+void build(int c, int s, int t)
 {
-    long long add = lazy_add[c];
-    long long set = lazy_set[c];
-    if (add != 0)
+    lazy_change[c] = Diff;
+    if (s == t)
     {
-        tree[c * 2] += add;
-        tree[c * 2 + 1] += add;
-        lazy_add[c * 2] += add;
-        lazy_add[c * 2 + 1] += add;
-    }
-    if (set != Diff)
-    {
-        tree[c * 2] = set;
-        tree[c * 2 + 1] = set;
-        lazy_set[c * 2] = set;
-        lazy_set[c * 2 + 1] = set;
-    }
-    lazy_add[c] = 0;
-    lazy_set[c] = Diff;
-}
-void changeAdd(int c, int s, int t, int l, int r, long long v)
-{
-    if (l <= s && t <= r)
-    {
-        tree[c] += v;
-        lazy_add[c] += v;
+        tree[c] = p[s];
         return;
     }
-    downstream(c, s, t);
     int mid = (s + t) / 2;
-    if (l <= mid)
-    {
-        changeAdd(c * 2, s, mid, l, r, v);
-    }
-    if (r > mid)
-    {
-        changeAdd(c * 2 + 1, mid + 1, t, l, r, v);
-    }
+    build(c * 2, s, mid);
+    build(c * 2 + 1, mid + 1, t);
     tree[c] = std::max(tree[c * 2], tree[c * 2 + 1]);
 }
-void changeSet(int c, int s, int t, int l, int r, long long v)
+void downstreamChange(int c, int s, int t)
+{
+    if (lazy_change[c] != Diff)
+    {
+        tree[c * 2] = lazy_change[c];
+        tree[c * 2 + 1] = lazy_change[c];
+        lazy_change[c * 2] = lazy_change[c];
+        lazy_change[c * 2 + 1] = lazy_change[c];
+        lazy_add[c * 2] = 0;
+        lazy_add[c * 2 + 1] = 0;
+        lazy_change[c] = Diff;
+    }
+}
+void downstreamAdd(int c, int s, int t)
+{
+    if (lazy_add[c] != 0)
+    {
+        tree[c * 2] += lazy_add[c];
+        if (lazy_change[c * 2] != Diff)
+        {
+            lazy_change[c * 2] += lazy_add[c];
+        }
+        else
+        {
+            lazy_add[c * 2] += lazy_add[c];
+        }
+        tree[c * 2 + 1] += lazy_add[c];
+        if (lazy_change[c * 2 + 1] != Diff)
+        {
+            lazy_change[c * 2 + 1] += lazy_add[c];
+        }
+        else
+        {
+            lazy_add[c * 2 + 1] += lazy_add[c];
+        }
+        lazy_add[c] = 0;
+    }
+}
+void downstream(int c, int s, int t)
+{
+    downstreamChange(c, s, t);
+    downstreamAdd(c, s, t);
+}
+void change(int c, int s, int t, int l, int r, long long v)
 {
     if (l <= s && t <= r)
     {
         tree[c] = v;
-        lazy_set[c] = v;
+        lazy_add[c] = 0;
+        lazy_change[c] = v;
         return;
     }
     downstream(c, s, t);
     int mid = (s + t) / 2;
     if (l <= mid)
     {
-        changeSet(c * 2, s, mid, l, r, v);
+        change(c * 2, s, mid, l, r, v);
     }
     if (r > mid)
     {
-        changeSet(c * 2 + 1, mid + 1, t, l, r, v);
+        change(c * 2 + 1, mid + 1, t, l, r, v);
+    }
+    tree[c] = std::max(tree[c * 2], tree[c * 2 + 1]);
+}
+void add(int c, int s, int t, int l, int r, long long v)
+{
+    if (l <= s && t <= r)
+    {
+        tree[c] += v;
+        if (lazy_change[c] != Diff)
+        {
+            lazy_change[c] += v;
+        }
+        else
+        {
+            lazy_add[c] += v;
+        }
+        return;
+    }
+    downstream(c, s, t);
+    int mid = (s + t) / 2;
+    if (l <= mid)
+    {
+        add(c * 2, s, mid, l, r, v);
+    }
+    if (r > mid)
+    {
+        add(c * 2 + 1, mid + 1, t, l, r, v);
     }
     tree[c] = std::max(tree[c * 2], tree[c * 2 + 1]);
 }
@@ -75,46 +119,42 @@ long long query(int c, int s, int t, int l, int r)
     }
     downstream(c, s, t);
     int mid = (s + t) / 2;
-    if (r <= mid)
+    long long res = -0x7f7f7f7f7f7f7f7f;
+    if (l <= mid)
     {
-        return query(c * 2, s, mid, l, r);
+        res = std::max(res, query(c * 2, s, mid, l, r));
     }
-    if (l > mid)
+    if (r > mid)
     {
-        return query(c * 2 + 1, mid + 1, t, l, r);
+        res = std::max(res, query(c * 2 + 1, mid + 1, t, l, r));
     }
-    return std::max(query(c * 2, s, mid, l, r), query(c * 2 + 1, mid + 1, t, l, r));
+    return res;
 }
 int main()
 {
-    scanf("%d%d", &n, &q);
+    scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++)
     {
-        long long x;
-        scanf("%lld", &x);
-        changeSet(1, 1, n, i, i, x);
+        scanf("%lld", &p[i]);
     }
-    for (int i = 1; i <= 4 * n; i++)
-    {
-        lazy_set[i] = Diff;
-    }
-    for (int i = 1; i <= q; i++)
+    build(1, 1, n);
+    for (int i = 1; i <= m; i++)
     {
         int op, l, r;
         scanf("%d%d%d", &op, &l, &r);
         if (op == 1)
         {
-            long long x;
-            scanf("%lld", &x);
-            changeSet(1, 1, n, l, r, x);
+            long long v;
+            scanf("%lld", &v);
+            change(1, 1, n, l, r, v);
         }
         else if (op == 2)
         {
-            long long x;
-            scanf("%lld", &x);
-            changeAdd(1, 1, n, l, r, x);
+            long long v;
+            scanf("%lld", &v);
+            add(1, 1, n, l, r, v);
         }
-        else
+        else if (op == 3)
         {
             printf("%lld\n", query(1, 1, n, l, r));
         }
