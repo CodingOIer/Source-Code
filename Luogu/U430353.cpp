14a5618e074132c15ae46e888731a33ee2a9dd70
CodingOIer <wanghongtiancodingoier@outlook.com>
2024-05-25 17:31:04 +0800

    afternoon

---
+++ Luogu/U430353.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+constexpr int MaxN = 2e5 + 5;
+int n, k;
+int p[MaxN];
+int main()
+{
+    scanf("%d%d", &n, &k);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &p[i]);
+    }
+    return 0;
+}
