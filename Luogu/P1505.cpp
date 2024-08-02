71d28034abb444ed08d20aa098635b40e6b81294
CodingOIer <wanghongtiancodingoier@outlook.com>
2024-08-02 17:01:53 +0800

    morning

--- Luogu/P1505.cpp
+++ Luogu/P1505.cpp
@@ -1,165 +1,125 @@
 #include <algorithm>
 #include <cstdio>
+#include <cstring>
 #include <map>
 #include <tuple>
 #include <vector>
 constexpr int MaxN = 2e5 + 5;
-int tree1[MaxN * 4];
-int tree2[MaxN * 4];
-int tree3[MaxN * 4];
-int lazy1[MaxN * 4];
-int lazy2[MaxN * 4];
-int lazy3[MaxN * 4];
-inline void downstream1(int c, int s, int t)
+int tree_sum[MaxN * 4];
+int tree_max[MaxN * 4];
+int tree_min[MaxN * 4];
+bool lazy[MaxN * 4];
+void downstream(int c, int s, int t)
 {
-    int mid = (s + t) / 2;
-    tree1[c * 2] += (mid - s + 1) * lazy1[c];
-    tree1[c * 2 + 1] += (t - s) * lazy1[c];
-    lazy1[c * 2] += lazy1[c];
-    lazy1[c * 2 + 1] += lazy1[c];
-    lazy1[c] = 0;
-}
-inline void downstream2(int c, int s, int t)
-{
-    int mid = (s + t) / 2;
-    tree2[c * 2] += lazy2[c];
-    tree2[c * 2 + 1] += lazy2[c];
-    lazy2[c * 2] += lazy2[c];
-    lazy2[c * 2 + 1] += lazy2[c];
-    lazy2[c] = 0;
-}
-inline void downstream3(int c, int s, int t)
-{
-    int mid = (s + t) / 2;
-    tree3[c * 2] += lazy3[c];
-    tree3[c * 2 + 1] += lazy3[c];
-    lazy3[c * 2] += lazy3[c];
-    lazy3[c * 2 + 1] += lazy3[c];
-    lazy3[c] = 0;
-}
-void change1(int c, int s, int t, int l, int r, int val)
-{
-    if (l <= s && t <= r)
+    if (lazy[c])
     {
-        tree1[c] += (t - s + 1) * val;
-        lazy1[c] += val;
+        int max, min;
+
+        tree_sum[c * 2] = -tree_sum[c * 2];
+        max = tree_max[c * 2];
+        min = tree_min[c * 2];
+        tree_max[c * 2] = -min;
+        tree_min[c * 2] = -max;
+
+        tree_sum[c * 2 + 1] = -tree_sum[c * 2 + 1];
+        max = tree_max[c * 2 + 1];
+        min = tree_min[c * 2 + 1];
+        tree_max[c * 2 + 1] = -min;
+        tree_min[c * 2 + 1] = -max;
+
+        lazy[c * 2] ^= true;
+        lazy[c * 2 + 1] ^= true;
+        lazy[c] = false;
+    }
+}
+void change(int c, int s, int t, int l, int r)
+{
+    if (l > r)
+    {
         return;
     }
-    downstream1(c, s, t);
-    int mid = (s + t) / 2;
-    if (l <= mid)
-    {
-        change1(c * 2, s, mid, l, r, val);
-    }
-    if (r > mid)
-    {
-        change1(c * 2 + 1, mid + 1, t, l, r, val);
-    }
-    tree1[c] = tree1[c * 2] + tree1[c * 2 + 1];
-}
-void change2(int c, int s, int t, int l, int r, int val)
-{
     if (l <= s && t <= r)
     {
-        tree2[c] += val;
-        lazy2[c] += val;
+        tree_sum[c] = -tree_sum[c];
+        int max, min;
+        max = tree_max[c];
+        min = tree_min[c];
+        tree_max[c] = -min;
+        tree_min[c] = -max;
+        lazy[c] ^= true;
+        return;
     }
-    downstream2(c, s, t);
+    downstream(c, s, t);
     int mid = (s + t) / 2;
     if (l <= mid)
     {
-        change2(c * 2, s, mid, l, r, val);
+        change(c * 2, s, mid, l, r);
     }
     if (r > mid)
     {
-        change2(c * 2 + 1, mid + 1, t, l, r, val);
+        change(c * 2 + 1, mid + 1, t, l, r);
     }
-    tree2[c] = std::max(tree2[c * 2], tree2[c * 2 + 1]);
+    tree_sum[c] = tree_sum[c * 2] + tree_sum[c * 2 + 1];
+    tree_max[c] = std::max(tree_max[c * 2], tree_max[c * 2 + 1]);
+    tree_min[c] = std::min(tree_min[c * 2], tree_min[c * 2 + 1]);
 }
-void change3(int c, int s, int t, int l, int r, int val)
+void change_single(int c, int s, int t, int w, int val)
 {
+    if (s == t)
+    {
+        tree_sum[c] = val;
+        tree_max[c] = val;
+        tree_min[c] = val;
+        return;
+    }
+    downstream(c, s, t);
+    int mid = (s + t) / 2;
+    if (w <= mid)
+    {
+        change_single(c * 2, s, mid, w, val);
+    }
+    else
+    {
+        change_single(c * 2 + 1, mid + 1, t, w, val);
+    }
+    tree_sum[c] = tree_sum[c * 2] + tree_sum[c * 2 + 1];
+    tree_max[c] = std::max(tree_max[c * 2], tree_max[c * 2 + 1]);
+    tree_min[c] = std::min(tree_min[c * 2], tree_min[c * 2 + 1]);
+}
+std::tuple<int, int, int> query(int c, int s, int t, int l, int r)
+{
+    if (l > r)
+    {
+        return {0, -0x7f7f7f7f, 0x7f7f7f7f};
+    }
     if (l <= s && t <= r)
     {
-        tree3[c] += val;
-        lazy3[c] += val;
+        return {tree_sum[c], tree_max[c], tree_min[c]};
     }
-    downstream3(c, s, t);
+    downstream(c, s, t);
     int mid = (s + t) / 2;
+    int x, y, z;
+    x = 0;
+    y = -0x7f7f7f7f;
+    z = 0x7f7f7f7f;
     if (l <= mid)
     {
-        change3(c * 2, s, mid, l, r, val);
+        auto [kx, ky, kz] = query(c * 2, s, mid, l, r);
+        x += kx;
+        y = std::max(y, ky);
+        z = std::min(z, kz);
     }
     if (r > mid)
     {
-        change3(c * 2 + 1, mid + 1, t, l, r, val);
+        auto [kx, ky, kz] = query(c * 2 + 1, mid + 1, t, l, r);
+        x += kx;
+        y = std::max(y, ky);
+        z = std::min(z, kz);
     }
-    tree3[c] = std::min(tree3[c * 2], tree3[c * 2 + 1]);
-}
-int query1(int c, int s, int t, int l, int r)
-{
-    if (l <= s && t <= r)
-    {
-        return tree1[c];
-    }
-    downstream1(c, s, t);
-    int mid = (s + t) / 2;
-    int response = 0;
-    if (l <= mid)
-    {
-        response += query1(c * 2, s, mid, l, r);
-    }
-    if (r > mid)
-    {
-        response += query1(c * 2 + 1, mid + 1, t, l, r);
-    }
-    return response;
-}
-int query2(int c, int s, int t, int l, int r)
-{
-    if (l <= s && t <= r)
-    {
-        return tree2[c];
-    }
-    downstream2(c, s, t);
-    int mid = (s + t) / 2;
-    int response = -0x7f7f7f7f;
-    if (l <= mid)
-    {
-        response = std::max(response, query2(c * 2, s, mid, l, r));
-    }
-    if (r > mid)
-    {
-        response = std::max(response, query2(c * 2 + 1, mid + 1, t, l, r));
-    }
-    return response;
-}
-int query3(int c, int s, int t, int l, int r)
-{
-    if (l <= s && t <= r)
-    {
-        return tree3[c];
-    }
-    downstream3(c, s, t);
-    int mid = (s + t) / 2;
-    int response = 0x7f7f7f7f;
-    if (l <= mid)
-    {
-        response = std::max(response, query3(c * 2, s, mid, l, r));
-    }
-    if (r > mid)
-    {
-        response = std::max(response, query3(c * 2 + 1, mid + 1, t, l, r));
-    }
-    return response;
-}
-void change(int c, int s, int t, int l, int r, int val)
-{
-    change1(c, s, t, l, r, val);
-    change2(c, s, t, l, r, val);
-    change3(c, s, t, l, r, val);
+    return {x, y, z};
 }
 int n, m;
-int index;
+int idx;
 int id[MaxN];
 int fa[MaxN];
 int top[MaxN];
@@ -167,6 +127,7 @@ int son[MaxN];
 int size[MaxN];
 int depth[MaxN];
 std::map<std::tuple<int, int>, int> ln;
+std::vector<std::tuple<int, int>> input;
 std::vector<std::tuple<int, int>> link[MaxN];
 void dfs_info(int u, int f)
 {
@@ -182,32 +143,77 @@ void dfs_info(int u, int f)
         dfs_info(v, u);
         if (size[v] > size[son[u]])
         {
-            son[u] = size[v];
+            son[u] = v;
         }
         size[u] += size[v];
     }
 }
 void dfs_split(int u, int tp)
 {
-    index++;
-    id[u] = index;
+    idx++;
+    id[u] = idx;
     top[u] = tp;
     if (son[u] != 0)
     {
-        ln[{u, son[u]}] = index + 1;
-        ln[{son[u], u}] = index + 1;
+        ln[{u, son[u]}] = idx + 1;
+        ln[{son[u], u}] = idx + 1;
         dfs_split(son[u], tp);
     }
     for (auto [v, _] : link[u])
     {
-        if (v == fa[u])
+        if (v == fa[u] || v == son[u])
         {
             continue;
         }
-        ln[{u, v}] = index + 1;
-        ln[{v, u}] = index + 1;
+        ln[{u, v}] = idx + 1;
+        ln[{v, u}] = idx + 1;
         dfs_split(v, v);
     }
+}
+void change(int x, int y)
+{
+    for (; top[x] != top[y];)
+    {
+        if (depth[top[x]] < depth[top[y]])
+        {
+            std::swap(x, y);
+        }
+        change(1, 1, n, id[top[x]], id[x]);
+        x = fa[top[x]];
+    }
+    if (depth[x] > depth[y])
+    {
+        std::swap(x, y);
+    }
+    change(1, 1, n, id[x] + 1, id[y]);
+}
+std::tuple<int, int, int> query(int x, int y)
+{
+    int a, b, c;
+    a = 0;
+    b = -0x7f7f7f7f;
+    c = 0x7f7f7f7f;
+    for (; top[x] != top[y];)
+    {
+        if (depth[top[x]] < depth[top[y]])
+        {
+            std::swap(x, y);
+        }
+        auto [kx, ky, kz] = query(1, 1, n, id[top[x]], id[x]);
+        a += kx;
+        b = std::max(b, ky);
+        c = std::min(c, kz);
+        x = fa[top[x]];
+    }
+    if (depth[x] > depth[y])
+    {
+        std::swap(x, y);
+    }
+    auto [kx, ky, kz] = query(1, 1, n, id[x] + 1, id[y]);
+    a += kx;
+    b = std::max(b, ky);
+    c = std::min(c, kz);
+    return {a, b, c};
 }
 int main()
 {
@@ -216,6 +222,9 @@ int main()
     {
         int u, v, w;
         scanf("%d%d%d", &u, &v, &w);
+        u++;
+        v++;
+        input.push_back({u, v});
         link[u].push_back({v, w});
         link[v].push_back({u, w});
     }
@@ -227,13 +236,50 @@ int main()
         {
             if (u < v)
             {
-                change(1, 1, n, ln[{u, v}], ln[{u, v}], w);
+                change_single(1, 1, n, ln[{u, v}], w);
             }
         }
     }
+    char opt[15];
     scanf("%d", &m);
     for (int i = 1; i <= m; i++)
     {
+        scanf("%s", opt);
+        if (strcmp(opt, "C") == 0)
+        {
+            int x, y;
+            scanf("%d%d", &x, &y);
+            auto [u, v] = input[x - 1];
+            change_single(1, 1, n, ln[{u, v}], y);
+        }
+        else if (strcmp(opt, "N") == 0)
+        {
+            int u, v;
+            scanf("%d%d", &u, &v);
+            u++;
+            v++;
+            change(u, v);
+        }
+        else
+        {
+            int u, v;
+            scanf("%d%d", &u, &v);
+            u++;
+            v++;
+            auto [x, y, z] = query(u, v);
+            if (strcmp(opt, "SUM") == 0)
+            {
+                printf("%d\n", x);
+            }
+            else if (strcmp(opt, "MAX") == 0)
+            {
+                printf("%d\n", y);
+            }
+            else if (strcmp(opt, "MIN") == 0)
+            {
+                printf("%d\n", z);
+            }
+        }
     }
     return 0;
 }
