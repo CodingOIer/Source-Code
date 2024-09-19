0094ed39f1fa9aef37a27a3e23f0fe06f3077182
CodingOIer <wanghongtiancodingoier@outlook.com>
2024-09-19 09:27:15 +0000

    [feat] B2006

--- Luogu/B2006.cpp
+++ Luogu/B2006.cpp
@@ -1,28 +1,8 @@
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <cmath>
-#include <complex>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <random>
-#include <set>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#define debug printf("Debug\n")
-typedef long long ll;
-const int inf = 0x3f3f3f3f;
+double x, a, y, b;
 int main()
 {
+    scanf("%lf%lf%lf%lf", &x, &a, &y, &b);
+    printf("%.2lf\n", (x * a - y * b) / (a - b));
     return 0;
 }
