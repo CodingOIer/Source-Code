2b046bfe87c4f2498b2bcd3cc644a377a4a1fec5
CodingOIer <wanghongtiancodingoier@outlook.com>
2024-09-25 17:03:45 +0800

    [feat] 一堆 GCD SUM 的板子题

---
+++ Luogu/UVA11426.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+constexpr int MaxN = 4e6 + 5;
+int n;
+long long p[MaxN];
+int main()
+{
+    for (;;)
+    {
+        scanf("%d", &n);
+        if (n == 0)
+        {
+            break;
+        }
+        long long answer = 0;
+        for (long long i = n; i >= 1; i--)
+        {
+            p[i] = (n / i) * (n / i);
+            for (long long j = i * 2; j <= n; j += i)
+            {
+                p[i] -= p[j];
+            }
+            answer += p[i] * i;
+            answer -= i;
+        }
+        printf("%lld\n", answer / 2);
+    }
+    return 0;
+}
