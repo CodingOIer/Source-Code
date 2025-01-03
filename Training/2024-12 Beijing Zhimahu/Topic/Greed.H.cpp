37bd9dcb3b88506f8a6e573ad6241c2903ed3848
CodingOIer <wanghongtiancodingoier@gmail.com>
2025-01-03 17:26:18 +0800

    :sparkles: 北京集训 贪心 H

---
+++ Training/2024-12 Beijing Zhimahu/Topic/Greed.H.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cstdio>
+#include <queue>
+#include <tuple>
+#include <vector>
+int n;
+std::priority_queue<long long> queue;
+std::vector<std::tuple<int, int>> v;
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        v.push_back({y, x});
+    }
+    std::sort(v.begin(), v.end());
+    int answer = 0;
+    long long cost = 0;
+    for (const auto &[y, x] : v)
+    {
+        cost += x;
+        queue.push(x);
+        answer++;
+        if (cost > y)
+        {
+            answer--;
+            cost -= queue.top();
+            queue.pop();
+        }
+    }
+    printf("%d\n", answer);
+    return 0;
+}
