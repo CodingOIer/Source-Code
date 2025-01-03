d1be8c7c7b477d492132ca4d8fce4b78c08a1e03
Hongtian Wang <i@codingoier.com>
2025-01-03 11:10:22 +0800

    :sparkles: P4688 完成 莫队

--- Problem/Luogu/P4688.cpp
+++ Problem/Luogu/P4688.cpp
@@ -2,18 +2,11 @@
 #include <bitset>
 #include <cmath>
 #include <cstdio>
+#include <cstring>
 #include <tuple>
 #include <vector>
-class ask
-{
-  public:
-    int l, r, id;
-    ask()
-    {
-    }
-};
 constexpr int MaxN = 1e5 + 5;
-constexpr int SubtaskSize = 1e2 + 5;
+constexpr int SubtaskSize = 3e4;
 int n, m;
 int p[MaxN];
 int l1[MaxN];
@@ -22,12 +15,55 @@ int l2[MaxN];
 int r2[MaxN];
 int l3[MaxN];
 int r3[MaxN];
+int cnt[MaxN];
+int have[MaxN];
+int begin[MaxN];
 std::vector<int> uq;
-std::bitset<MaxN> f[SubtaskSize * 3];
+std::bitset<MaxN> f[SubtaskSize + 5];
 std::vector<std::tuple<int, int, int>> v;
-void initMo()
+void solve()
 {
     int block = sqrt(v.size());
+    std::sort(v.begin(), v.end(), [&](const std::tuple<int, int, int> a, const std::tuple<int, int, int> b) {
+        return std::get<0>(a) / block == std::get<0>(b) / block
+                   ? (std::get<1>(a) == std::get<1>(b) ? false : std::get<0>(a) / block % 2 == 1) ^
+                         (std::get<1>(a) < std::get<1>(b))
+                   : std::get<0>(a) / block < std::get<0>(b) / block;
+    });
+    memset(have, 0, sizeof(have));
+    int l, r;
+    l = 1;
+    r = 0;
+    std::bitset<MaxN> res;
+    for (const auto &[wantL, wantR, id] : v)
+    {
+        for (; wantL < l;)
+        {
+            l--;
+            have[p[l]]++;
+            res[begin[p[l]] + have[p[l]] - 1] = true;
+        }
+        for (; r < wantR;)
+        {
+            r++;
+            have[p[r]]++;
+            res[begin[p[r]] + have[p[r]] - 1] = true;
+        }
+        for (; l < wantL;)
+        {
+            res[begin[p[l]] + have[p[l]] - 1] = false;
+            have[p[l]]--;
+            l++;
+        }
+        for (; wantR < r;)
+        {
+            res[begin[p[r]] + have[p[r]] - 1] = false;
+            have[p[r]]--;
+            r--;
+        }
+        f[id] = res;
+    }
+    v.clear();
 }
 int main()
 {
@@ -42,16 +78,47 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         p[i] = std::lower_bound(uq.begin(), uq.end(), p[i]) - uq.begin() + 1;
+        cnt[p[i]]++;
     }
-    int calcPre = 1;
+    int prefixCnt = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        begin[i] = prefixCnt + 1;
+        prefixCnt += cnt[i];
+    }
+    int cntOfMo = 0;
+    int lastCalc = 0;
     for (int i = 1; i <= m; i++)
     {
-        v.push_back({l1[i], r1[i], i * 3 - 2});
-        v.push_back({l2[i], r2[i], i * 3 - 1});
-        v.push_back({l3[i], r3[i], i * 3 - 0});
-        if (i - calcPre + 1 >= SubtaskSize)
+        scanf("%d%d%d%d%d%d", &l1[i], &r1[i], &l2[i], &r2[i], &l3[i], &r3[i]);
+        v.push_back({l1[i], r1[i], ++cntOfMo});
+        v.push_back({l2[i], r2[i], ++cntOfMo});
+        v.push_back({l3[i], r3[i], ++cntOfMo});
+        if (cntOfMo >= SubtaskSize)
         {
+            solve();
+            int ck = 0;
+            for (int j = lastCalc + 1; j <= i; j++)
+            {
+                int a, b, c;
+                a = ++ck;
+                b = ++ck;
+                c = ++ck;
+                printf("%zu\n", (f[a].count() + f[b].count() + f[c].count()) - 3 * (f[a] & f[b] & f[c]).count());
+            }
+            cntOfMo = 0;
+            lastCalc = i;
         }
+    }
+    solve();
+    int ck = 0;
+    for (int j = lastCalc + 1; j <= m; j++)
+    {
+        int a, b, c;
+        a = ++ck;
+        b = ++ck;
+        c = ++ck;
+        printf("%zu\n", (f[a].count() + f[b].count() + f[c].count()) - 3 * (f[a] & f[b] & f[c]).count());
     }
     return 0;
 }
