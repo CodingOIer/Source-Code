9bb1696e1176676ba354d4e90465fc8e1e50a9c2
Hongtian Wang <i@codingoier.com>
2025-01-05 21:19:26 +0800

    :bug: CQYC20250105 IOI 赛制 代码错位:

--- CQYC/2025/0105/C.cpp
+++ CQYC/2025/0105/C.cpp
@@ -1,107 +1,54 @@
+#include <algorithm>
 #include <cstdio>
-#include <map>
-#include <random>
-#include <vector>
-std::mt19937 rnd(std::random_device{}());
-constexpr int MaxN = 5e2 + 5;
-int n;
-long long Mod;
-unsigned long long base[MaxN];
-std::map<unsigned long long, bool> vis;
-std::map<unsigned long long, bool> map;
-std::vector<std::vector<long long>> v;
-unsigned long long fsp(unsigned long long x, long long y)
-{
-    unsigned long long res = 1;
-    for (; y != 0;)
-    {
-        if (y & 1)
-        {
-            res = res * x % Mod;
-        }
-        x = x * x % Mod;
-        y >>= 1;
-    }
-    return res;
-}
-unsigned long long getHash(const std::vector<long long> &v)
-{
-    int cnt = 0;
-    unsigned long long res = 0;
-    for (const auto &val : v)
-    {
-        res += fsp(val + 975432499, base[++cnt]);
-    }
-    return res;
-}
-void dfs(const std::vector<std::vector<long long>> &v)
-{
-    if (v.size() == 1)
-    {
-        map[getHash(v[0])] = true;
-        return;
-    }
-    unsigned long long allHash = 0;
-    for (int i = 0; i < int(v.size()); i++)
-    {
-        allHash += getHash(v[i]);
-    }
-    if (vis[allHash])
-    {
-        return;
-    }
-    vis[allHash] = true;
-    for (int i = 0; i < int(v.size()); i++)
-    {
-        for (int j = i + 1; j < int(v.size()); j++)
-        {
-            std::vector<std::vector<long long>> cp;
-            for (int k = 0; k < int(v.size()); k++)
-            {
-                if (k != i && k != j)
-                {
-                    cp.push_back(v[k]);
-                }
-            }
-            std::vector<long long> temp;
-            for (int k = 0; k < n; k++)
-            {
-                temp.push_back({v[j][k] + v[j][k] - v[i][k]});
-            }
-            cp.push_back(temp);
-            dfs(cp);
-            cp.pop_back();
-            temp.clear();
-            for (int k = 0; k < n; k++)
-            {
-                temp.push_back({v[i][k] + v[i][k] - v[j][k]});
-            }
-            cp.push_back(temp);
-            dfs(cp);
-        }
-    }
-}
+#include <cstring>
+constexpr int MaxN = 1e3 + 5;
+int n, m;
+int a[MaxN];
+int b[MaxN];
+long long dp[MaxN][MaxN];
+long long best[MaxN][MaxN];
 int main()
 {
-    scanf("%d%lld", &n, &Mod);
+    scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++)
     {
-        base[i] = rnd();
-        std::vector<long long> temp;
-        for (int j = 1; j <= n; j++)
+        scanf("%d", &a[i]);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    memset(best, 0x3f, sizeof(best));
+    best[0][0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= std::min(i, m); j++)
         {
-            if (i == j)
+            for (int k = 0; k <= j; k++)
             {
-                temp.push_back(1);
-            }
-            else
-            {
-                temp.push_back(0);
+                dp[j][k] = best[j][k];
+                if (j >= 1)
+                {
+                    dp[j][k] = std::min(dp[j][k], best[j - 1][k] + a[i]);
+                }
+                if (k >= 1)
+                {
+                    dp[j][k] = std::min(dp[j][k], best[j][k - 1] + b[i]);
+                }
+                if (j >= 1 && k >= 1)
+                {
+                    dp[j][k] = std::min(dp[j][k], best[j - 1][k - 1] + a[i] + b[i]);
+                }
             }
         }
-        v.push_back(temp);
+        for (int j = 0; j <= std::min(i, m); j++)
+        {
+            for (int k = 0; k <= j; k++)
+            {
+                best[j][k] = std::min(best[j][k], dp[j][k]);
+            }
+        }
     }
-    dfs(v);
-    printf("%zu\n", map.size());
+    printf("%lld\n", best[m][m]);
     return 0;
 }
